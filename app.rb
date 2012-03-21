05ee7e92fac6744641c49364e6b6461c0d134471
Ian Vaughan <github@ianvaughan.co.uk>
2012-03-21 16:10:26 +0000

    start of using correct config

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require_relative 'forerunner'
+
+enable :sessions
+
+default_projects = %w{cas wld-api-router wld-service-site portal portal-sites wld-service-communication wld-service-member wld-service-search mobile}
+
+get '/' do
+  session[:runner] = Forerunner.new(Dir.home + '/Projects/')
+  @projects = default_projects
+  erb :index
+end
+
+post '/start' do
+  list = params.each_key.collect { |x| x }
+  session[:runner].boot list
+end
---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+require 'app.rb'
+
+# Run the app!
+run Sinatra::Application
