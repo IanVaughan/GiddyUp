f1034a019408839ed090c6e1ab87392c7e1d247c
Ian Vaughan <github@ianvaughan.co.uk>
2012-03-22 11:33:00 +0000

    correct command line version for new paths

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/ruby
-require_relative 'forerunner'
+require_relative '../lib/forerunner'
 
 projects=%w{cas wld-api-router wld-service-site portal portal-sites wld-service-communication wld-service-member wld-service-search mobile}
 
 fr = Forerunner.new Dir.home + '/Projects/'
 #fr.start 'wld-api-router'
-fr.boot projects
+fr.boot_up projects
 fr.list
 
