c39d74b94c9121d41b143175dada775d8f5e68b6
Ian Vaughan <github@ianvaughan.co.uk>
2012-04-20 17:59:20 +0100

    debug on server methods/entry points

--- lib/giddyup/server.rb
+++ lib/giddyup/server.rb
@@ -19,14 +19,18 @@ module GiddyUp
     end
 
     get '/list/:path' do
+      GiddyUp.logger.debug '/list'
       puts " #{params[:path]}"
     end
 
     get '/die' do
+      GiddyUp.logger.debug '/die'
       redirect to('/')
     end
 
     post '/perform' do
+      GiddyUp.logger.debug '/perform'
+
       GiddyUp.launcher.start params.each_pair.collect { |key, value| key if value == 'start' }.compact
       GiddyUp.launcher.stop params.each_pair.collect { |key, value| key if value == 'stop' }.compact
     end
